74725957c0c88e8123aec30de0bd47a8def2a6f9
Mike Tsao <mike@sowbug.com>
2014-08-18 20:53:59 -0700

    Orange. Constants. Start of progress bar.

--- clockgif.py
+++ clockgif.py
@@ -2,13 +2,18 @@
 
 from subprocess import call
 
+SECONDS = 15
+SLIDES = 20
+
 frame_filenames = []
-for x in range(0, 15):
+for x in range(0, SECONDS):
     frame_filename = "countdown-%d.gif" % x
-    if x < 10:
-        color = "SpringGreen"
-    elif x < 13:
+    if x < SECONDS - 5:
+        color = "green1"
+    elif x < SECONDS - 2:
         color = "yellow"
+    elif x < SECONDS - 1:
+        color = "orange"
     else:
         color = "red"
     call(["convert",
@@ -19,13 +24,15 @@ for x in range(0, 15):
           "-pointsize", "24",
           "-border", "8x8",
           "label:00:%02d" % (15 - x),
+#          "-fill", "yellow",
+#          "-draw", "rectangle 0,0 %d,4" % (93 * (x + 1) / SLIDES),
           frame_filename])
     frame_filenames.append(frame_filename)
 
 args = ["convert",
         "-delay", "100",
         "-loop", "1"]
-for x in range(0, 20):
+for x in range(0, SLIDES):
     args += frame_filenames
 args.append("clock.gif")
 call(args)
